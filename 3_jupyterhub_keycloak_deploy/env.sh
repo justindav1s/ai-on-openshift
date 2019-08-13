288b8e7d9d5afbfdcafcccebd0a5916ef04b9032
Justin Davis <justinndavis@gmail.com>
2019-08-13 16:55:30 +0100

    serving updates

--- 3_jupyterhub_keycloak_deploy/env.sh
+++ 3_jupyterhub_keycloak_deploy/env.sh
@@ -2,8 +2,7 @@
 
 export OCP=ocp.datr.eu
 export USER=justin
-export PROJECT=jupyterhub-keycloak2
+export PROJECT=jupyterhub-keycloak
 export NOTEBBOKS_NS=jupyter-notebooks
 export JUPYTERHUB_VERSION=3.1.0
-#export NOTEBOOK_IMAGE="docker-registry.default.svc:5000/${NOTEBBOKS_NS}/minimal-nvidia-notebook:latest"
 export NOTEBOOK_IMAGE="docker-registry.default.svc:5000/${NOTEBBOKS_NS}/tensorflow-nvidia-notebook:latest"
