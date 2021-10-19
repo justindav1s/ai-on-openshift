3206efb20ab51e21eb81c590ddb239a1a4364e47
Justin Davis <justinndavis@gmail.com>
2021-10-19 18:58:11 +0100

    Update build-tensorflow.sh

--- 2_nvidia_jupyter_notebook_build/tensorflow-notebook/build-tensorflow.sh
+++ 2_nvidia_jupyter_notebook_build/tensorflow-notebook/build-tensorflow.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 oc new-build --name tensorflow-nvidia-notebook \
-  --image-stream s2i-nvidia-minimal-notebook:latest \
+  --image-stream s2i-nvidia-notebook:latest \
   --code https://github.com/justindav1s/ai-on-openshift.git \
   --context-dir 2_nvidia_jupyter_notebook_build/tensorflow-notebook
