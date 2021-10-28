e57cec062cf933f31eb76769aa791d1adf7f8a20
Justin Davis <justinndavis@gmail.com>
2021-10-28 10:14:15 +0100

    refactoring

--- 2_gpu_jupyter_notebook_build/notebook-base-image/build.sh
+++ 2_gpu_jupyter_notebook_build/notebook-base-image/build.sh
@@ -5,7 +5,7 @@
 APPLICATION_NAME="s2i-nvidia-notebook"
 SOURCE_REPOSITORY_URL="https://github.com/justindav1s/ai-on-openshift.git"
 SOURCE_REPOSITORY_REF="master"
-REPO_PATH="2_gpu_jupyter_notebook_build/s2i-notebook-image/minimal-notebook"
+REPO_PATH="2_gpu_jupyter_notebook_build/notebook-base-image/minimal-notebook"
 BASE_IMAGE="quay.io/justindav1s/gpu-base:latest"
 BASE_IMAGE_NS="jupyter-notebooks"
 
