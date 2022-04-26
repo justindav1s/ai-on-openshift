d8ed9b79a9f4dc07432f7f1516b7a073b8f9855a
Justin Davis <justinndavis@gmail.com>
2022-04-26 14:52:33 +0100

    updates

---
+++ 3.1_jupyterhub_keycloak_deploy/build-notebook.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+. ./env.sh
+
+APPLICATION_NAME=jupyterhub
+
+oc project ${PROJECT}
+cd templates
+
+oc delete is ${APPLICATION_NAME}-nb-img
+oc delete bc ${APPLICATION_NAME}-nb-img
+
+oc new-app notebook-bc.yaml \
+   -p APPLICATION_NAME=${APPLICATION_NAME} \
+   -p NOTEBOOK_REPOSITORY_URL=https://github.com/justindav1s/ai-on-openshift \
+   -p NOTEBOOK_REPOSITORY_CONTEXT_DIR=2_gpu_jupyter_notebook_build/tensorflow-notebook \
+   -p BASE_IMAGE=quay.io/justindav1s/gpu-notebook-base:latest
+
+sleep 5
+
+oc logs -f buildconfig/jupyterhub-nb-img
