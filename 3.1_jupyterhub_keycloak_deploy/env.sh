5629cd7d7322853488f7f849ba3c1e0f0413c4e6
Justin Davis <justinndavis@gmail.com>
2022-04-27 09:24:17 +0100

    cleanup

--- 3.1_jupyterhub_keycloak_deploy/env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export OCP=api.sno.openshiftlabs.net:6443
-export USER=justin
-export PROJECT=jupyterhub
-export NOTEBBOKS_NS=jupyter-notebooks
-export JUPYTERHUB_VERSION=3.1.0
-export NOTEBOOK_IMAGE="docker-registry.default.svc:5000/${NOTEBBOKS_NS}/tensorflow-nvidia-notebook:latest"
